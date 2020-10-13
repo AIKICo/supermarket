10b6abac8f7bbb40f9c4af5b8cad4f8a61c53e28
nahidmonjezi@gmail.com <nahidmonjezi@gmail.com>
2020-10-13 12:54:46 +0330

    Initail

---
+++ AIKI.CO.Supermarket.WebApi/Models/dbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AIKI.CO.Supermarket.WebApi.Models
+{
+    public class dbContext:DbContext
+    {
+        public dbContext(DbContextOptions options):base(options)
+        {
+          
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.EnableSensitiveDataLogging();
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.HasDefaultSchema("public");
+        }
+    }
+}
